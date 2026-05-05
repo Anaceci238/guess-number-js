35a09eed17ec21e646af92d1a50eadf1c35461ef
Ana Cecilia <anita19ceci@gmail.com>
2026-05-04 22:55:54 -0500

    laboratorio9

---
+++ script.js
@@ -0,0 +1,34 @@
+let nombre = "Ana";
+let edad = 32
+let estoyAprendiendo ="programación"
+console.log(nombre)
+console.log(edad)
+console.log(estoyAprendiendo)
+
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof estoyAprendiendo)
+
+const curso = "Code 101";
+const maxIntentos = 10;
+console.log(curso);
+console.log(maxIntentos);
+
+let nombreUsuario = prompt('¿Cómo te llamas?');
+console.log(nombreUsuario);
+
+let a = 20;
+let b = 7;
+
+console.log('Suma:', a + b);        // 27
+console.log('Resta:', a - b);       // 13
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+let saludo1 = 'Hola ' + nombreUsuario + ', tienes ' + edadUsuario + ' años.';
+console.log(saludo1);
+
+let saludo2= `Hola ${nombreUsuario} tienes ${edadUsuario} años`;
+
+
