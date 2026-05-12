e0fe01f260f58ef981a6407f2e72b2465df154d3
Ana Cecilia <anita19ceci@gmail.com>
2026-05-11 22:00:54 -0500

    feat: interfaz html

--- script.js
+++ script.js
@@ -1,63 +1,22 @@
-/*let nombre = "Ana";
-let edad = 32
-let estoyAprendiendo ="programación"
-console.log(nombre)
-console.log(edad)
-console.log(estoyAprendiendo)
+// === ADIVINA EL NÚMERO - Versión DOM ===
 
-console.log(typeof nombre);
-console.log(typeof edad);
-console.log(typeof estoyAprendiendo)
+// --- Seleccionar elementos del HTML ---
+const inputIntento = document.getElementById('inputIntento');
+const btnAdivinar = document.getElementById('btnAdivinar');
+const mensaje = document.getElementById('mensaje');
+const contador = document.getElementById('contador');
+const historial = document.getElementById('historial');
+const btnReiniciar = document.getElementById('btnReiniciar');
+const tarjeta = document.getElementById('game-card');
 
-const curso = "Code 101";
-const maxIntentos = 20;
-console.log(curso);
-console.log(maxIntentos);
+console.log('Elementos conectados:', inputIntento, btnAdivinar, mensaje);
 
-let nombreUsuario = prompt('¿Cómo te llamas?');
-console.log(nombreUsuario);
 
-let edadUsuario = prompt("¿Cuántos años tienes?");
-console.log(edadUsuario);
-
-let a = 20;
-let b = 7;
-
-console.log('Suma:', a + b);        // 27
-console.log('Resta:', a - b);       // 13
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
-
-let saludo1 = 'Hola ' + nombreUsuario + ', tienes ' + edadUsuario + ' años.';
-console.log(saludo1);
-
-let saludo2= `Hola ${nombreUsuario} tienes ${edadUsuario} años`;
-console.log(saludo2);
-
-// --- Calculadora de edad ---
-let anioActual = 2026;
-let anioNacimiento = anioActual - Number(edadUsuario);
-
-// Completa el mensaje usando template literals:
-// Debe decir: "Hola [nombre], naciste aproximadamente en [año]"
-let mensaje= `Hola ${nombreUsuario}, naciste aproximadamente en ${anioNacimiento}`;
-console.log(mensaje);
-
-*/
-
-let total = parseFloat(prompt("Ingresa el precio total:"));
-
-let precioFinal;
-
-if (total > 100) {
-  precioFinal = total * 0.80; // 20% de descuento
-} else if (total >= 50 && total <= 100) {
-  precioFinal = total * 0.90; // 10% de descuento
-} else {
-  precioFinal = total; // Sin descuento
+// --- Tu primera función ---
+function mostrarMensaje(texto, color) {
+  mensaje.textContent = texto;
+  mensaje.style.color = color;
 }
 
-// Mostrar resultado
-alert("Tu precio final es: $" + precioFinal);
-
+// Prueba la función
+mostrarMensaje('¡Bienvenido al juego!', '#e94560');
